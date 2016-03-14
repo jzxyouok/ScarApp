664e2eaf5e31cfb59ef3213a2fbadff33f5df0e8
zhengyunfei <zhengyf@ctyun.cn>
2016-03-15 00:28:40 +0800

    课程表初始显示

--- web/res/js/currenttime.js
+++ web/res/js/currenttime.js
@@ -1,7 +1,11 @@
 /**
  * Created by Administrator on 2016/3/11.
  */
+var currentWeek="";
+var currentWeekIndex="";
+
 showTime();
+
 function showTime(){
     var now= new Date();//获取当前时间
     var year=now.getFullYear();//年
@@ -12,20 +16,43 @@ function showTime(){
     var minute=now.getMinutes();//分
     var seconds=now.getSeconds();    //获取当前秒数(0-59)
     var milliseconds=now.getMilliseconds();   //获取当前毫秒数(0-999)
-    if(now == 0)
-        day=" 星期日";
-    else if(day == 1)
-        day=" 星期一";
-    else if(day == 2)
-        day=" 星期二";
-    else if(day == 3)
-        day=" 星期三";
-    else if(day == 4)
-        day=" 星期四";
-    else if(day == 5)
+    if(now == 0) {
+        day = " 星期日";
+        currentWeek = "周日";
+        currentWeekIndex=6;
+    }
+    else if(day == 1) {
+
+        day = " 星期一";
+        currentWeek = "周一";
+        currentWeekIndex=0;
+    }
+    else if(day == 2) {
+        day = " 星期二";
+        currentWeek="周二";
+        currentWeekIndex=1;
+    }
+    else if(day == 3) {
+        day = " 星期三";
+        currentWeek="周三";
+        currentWeekIndex=2;
+    }
+    else if(day == 4) {
+        day = " 星期四";
+        currentWeek="周四";
+        currentWeekIndex=3;
+    }
+    else if(day == 5){
         day=" 星期五";
-    else if(day == 6)
-        day=" 星期六";
+        currentWeek="周五";
+        currentWeekIndex=4;
+
+    }
+    else if(day == 6) {
+        day = " 星期六";
+        currentWeek="周六";
+        currentWeekIndex=5;
+    }
     if(hour<10)hour="0"+hour;
     if(minute<10)minute="0"+minute;
     if(date<10)date="0"+date;
