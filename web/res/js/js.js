6e8b3a3770fddd6d841e92c2bfbd781a597a0af6
zhengyunfei <zhengyf@ctyun.cn>
2016-03-21 10:12:55 +0800

    解决在ubuntu操作系统下，字体加载不了，页面访问字体一直加载不了的情况，是因为字体文件格式在ubuntu系统的不支持

--- web/res/js/js.js
+++ web/res/js/js.js
@@ -1,13 +1,10 @@
-﻿
-(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
+﻿(function (doc, win) {
+    var docEl = doc.documentElement,resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    var  recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
             docEl.style.fontSize = 100 * (clientWidth / 656) + 'px';
         };
-    // Abort if browser does not support addEventListener
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
--- web/res/js/public.js
+++ web/res/js/public.js
@@ -28,15 +28,10 @@ $(document).ready(
                 var url = $(this).attr("url");
                 var type=$(this).attr("id");
                 if (url) {
-                    window.location.href = window.location.protocol+"//" + window.location.host + url + "?" + new Date().getTime();
+                    window.location.href = window.location.protocol+"//" + window.location.host + url;
                 }
-               // if ($(this).attr("id") != "seats") {
-                   // $("#seats").data("seatsState", "none");
-                  //  $("#divSeats").slideUp();
-               // }
             }
         });
-        //payByCardRead();
     });
 
 var nums = ["一", "二", "三", "四", "五", "六", "七", "八", "九"];
@@ -50,52 +45,5 @@ function setCurrentDate() {
     $("#currentDate").text(curDate.Format("yyyy-MM-dd"));
 }
 
-
-function AddDays(date, days) {
-    return new Date(Date.parse(date) + (days * 24 * 60 * 60 * 1000));
-}
-//Date.prototype.AddDays = function (days) {
-//    return new Date(Date.parse(this) + (days * 24 * 60 * 60 * 1000));
-//}
-//String.prototype.AddDays = function (days) {
-//    return new Date(Date.parse(this.replace(/-/g, '/')) + (days * 24 * 60 * 60 * 1000));
-//}
-// 对Date的扩展，将 Date 转化为指定格式的String
-// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
-// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
-// 例子：
-// (new Date()).Format("yyyy-MM-dd HH:mm:ss.S") ==> 2006-07-02 08:09:04.423
-// (new Date()).Format("yyyy-M-d H:m:s.S")      ==> 2006-7-2 8:9:4.18
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "H+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds() //毫秒
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
-String.prototype.Format = function (fmt) {
-    var dateResult = new Date(this.replace(/-/g, '/'));
-    var o = {
-        "M+": dateResult.getMonth() + 1, //月份
-        "d+": dateResult.getDate(), //日
-        "H+": dateResult.getHours(), //小时
-        "m+": dateResult.getMinutes(), //分
-        "s+": dateResult.getSeconds(), //秒
-        "q+": Math.floor((dateResult.getMonth() + 3) / 3), //季度
-        "S": dateResult.getMilliseconds() //毫秒
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (dateResult.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
 
 
