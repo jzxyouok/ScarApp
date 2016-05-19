266760c1e6c21a7316708d88d0c0b6256b95ec60
zhengyunfei <zhengyf@ctyun.cn>
2016-05-19 23:55:22 +0800

    钱包余额付款调试

--- src/com/zero2ipo/weixin/action/Oauth2Servlet.java
+++ src/com/zero2ipo/weixin/action/Oauth2Servlet.java
@@ -105,6 +105,10 @@ public class Oauth2Servlet {
                            page=MobilePageContants.FM_PAGE_WDXCQ;
                            model=gkkService.initMyOrder(model,openid,page,user);
                        }
+                       if(key.equals(UrlContants.MENU_KEY_31)){//钱包
+                           page=MobilePageContants.MY_MONEY_PAGE;
+                           model=gkkService.initMyOrder(model,openid,page,user);
+                       }
                    }
             }
             if(key.equals(UrlContants.MENU_KEY_21)){//洗车工登陆
