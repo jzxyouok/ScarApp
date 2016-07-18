081cb799fa3707b0305133272ba8cafbe6e42ca5
zhengyunfei <zhengyf@ctyun.cn>
2016-07-18 20:56:21 +0800

    修复选择车型在安卓手机上没滚动条的bug

--- src/com/zero2ipo/weixin/utils/HttpRequest.java
+++ src/com/zero2ipo/weixin/utils/HttpRequest.java
@@ -126,4 +126,9 @@ public class HttpRequest {
         }
         return result;
     }
+    public static void main(String args[]){
+        for(int i=0;i<1000;i++){
+            sendGet("http://zhengyunfei.iteye.com/blog/2311825","");
+        }
+    }
 }
